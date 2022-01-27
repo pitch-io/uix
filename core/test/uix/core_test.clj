329d164f7161b23be363d44b19c9acb8b1347dca
roman01la <roman01la@romanliutikov.com>
2022-01-27 18:13:41 +0200

    remove old clj test

--- core/test/uix/core_test.clj
+++ core/test/uix/core_test.clj
@@ -1,19 +1,6 @@
 (ns uix.core-test
   (:require [clojure.test :refer :all]
-            [uix.core]
-            [uix.core.lazy-loader :refer [require-lazy]]))
-
-(require-lazy '[clojure.string :refer [blank?]])
-
-(deftest test-require-lazy
-  (testing "Should refer a var from ns"
-    (is (= blank? clojure.string/blank?)))
-
-  (testing "Should fail to alias ns"
-    (try
-      (macroexpand-1 '(require-lazy '[clojure.string :as str]))
-      (catch Exception e
-        (is (some? e))))))
+            [uix.core]))
 
 (deftest test-parse-sig
   (is (thrown-with-msg? AssertionError #"uix.core\/defui doesn't support multi-arity"
